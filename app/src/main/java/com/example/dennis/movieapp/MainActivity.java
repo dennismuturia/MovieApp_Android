c34b7ef89d32987980288b1da43337631b8d3b4c
dennismuturia <dmuturia2922@gmail.com>
2017-09-14 11:04:17 +0300

    Created another activity movie
    In this activity i got an intent that had the data from the main activity and displays it in this activity. Also added a few colors to the color .xml

--- app/src/main/java/com/example/dennis/movieapp/MainActivity.java
+++ app/src/main/java/com/example/dennis/movieapp/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.dennis.movieapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    //Lets start by binding
+    @Bind(R.id.thisEditText)EditText myEditText;
+    @Bind(R.id.movieButton)Button myButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Lets implement a butterknife
+        ButterKnife.bind(this);
+
+        //Lets set an onclick listener and implement an Explicit intent
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String movie = myEditText.getText().toString();
+                Intent intent = new Intent(MainActivity.this, MovieActivity.class);
+                intent.putExtra("movie", movie);
+                startActivity(intent);
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/dennis/movieapp/MovieActivity.java
+++ app/src/main/java/com/example/dennis/movieapp/MovieActivity.java
@@ -1,13 +1,28 @@
 package com.example.dennis.movieapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MovieActivity extends AppCompatActivity {
+//This is a bind for the text view
+    @Bind(R.id.displayText)TextView Display;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie);
+
+        //Use butter Knife
+        ButterKnife.bind(this);
+
+        //Lets Collect the data from the main activity
+        Intent intent = getIntent();
+        String movie = intent.getStringExtra("movie");
+        Display.setText("Movie: " + movie);
     }
 }
